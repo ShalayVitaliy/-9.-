7fc26eddf92140feced90a81deeda657af21834e
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-04-07 14:35:17 +0300

    Done Exercise001

---
+++ Exercise001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string OutPutNumber(int num)
+{
+    if(num == 1)
+        return 1.ToString();
+    return  num.ToString() + ", " + OutPutNumber(num - 1);
+}
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+Console.Clear();
+int Number = Prompt("Enter a number: ");
+Console.Write(OutPutNumber(Number));
